7b84746982fd344505055d2d92a11df25787e1e1
LanguorUna <karina_bazretdinovna@mail.ru>
2023-11-27 01:37:43 +0500

    fix paths

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,7 @@ function Navigation() {
     const items = ['Главная', 'Про гида', 'Программа тура', 'Стоимость', 'Блог', 'Контакты']
     return (
         <div className="flex justify-center xl:justify-between items-center flex-wrap gap-12">
-            <img src="src/assets/logo.png" alt="logo" className="cursor-pointer"/>
+            <img src="../assets/logo.png" alt="logo" className="cursor-pointer"/>
             <div className="flex justify-center gap-12 lg:justify-between items-baseline flex-wrap">
                 <div className="text-lg flex gap-6 items-baseline flex-wrap justify-center">
                     {items.map((item) => (
